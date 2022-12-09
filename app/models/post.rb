413306f780afbe30a0896b62c3dca4fe32c14e5a
Kane Mathis <admin@kanemathis.com>
2022-12-09 12:18:19 -0500

    commit tree 41091896713ec41573a62690c40e0a4ef71ccdc3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,37 @@
+require "pry"
 class Post < ApplicationRecord
+    
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {in: 0..250}
+    validates :category, inclusion:  {in: %w(Fiction Non-Fiction)}
+
+    # validate :click_bait
+
+    # # custom validation----
+    
+    # def click_bait
+    #     # binding.pry
+    #     unless title.match?("Won't Believe") || title.match?("Secret") || title.match?("Guess")
+    #         errors.add(:title, "title not dumb enough")
+    #     end
+    # end
+
+    validate :clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+    
 end
+
+
